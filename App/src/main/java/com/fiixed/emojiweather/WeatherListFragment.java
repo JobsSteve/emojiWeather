037fb73038ae8d88d3a990830ad6a7e93c3c2912
Andrew Bell <fiixed@gmail.com>
2013-12-12 10:38:48 -0600

    final commit before restructure

--- App/src/main/java/com/fiixed/emojiweather/WeatherListFragment.java
+++ App/src/main/java/com/fiixed/emojiweather/WeatherListFragment.java
@@ -17,9 +17,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
